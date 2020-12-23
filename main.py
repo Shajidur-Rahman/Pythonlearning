d34a67a3ed8575bcb0f269cc345c06ddb2ceb46e
Shajidur Rahman <shajidurrahmansaad10@gmail.com>
2020-12-22 21:46:31 -0800

    i learn at 11:45 am in 23/12/2020

--- main.py
+++ main.py
@@ -34,11 +34,39 @@ b = a.endswith(".")
 
 #go to the "https://www.w3schools.com/python/ref_string_expandtabs.asp" link to know expendsteg
 
-#using find
+#using find it is same to index
 a = "welcome to my website"
 b = a.find("welcome")
 #if you have the velue it will show you the index number
 #print(b)
 #if you dont have it will show you -1
 
-#
+#check all isalnum
+a = "mynameissaad"
+b = a.isalnum()
+#the result will be true
+#print(b)
+
+a = "my name is saad"
+b = a.isalnum()
+#the result will be false
+#because there is some space
+#print(b)
+
+# using isalpha
+a = "shajidur"
+b = a.isalpha()
+#Check if all the characters in the text is alphabetic
+#print(b)
+
+#using identifier
+a = "shajidurrahman"
+b = a.isidentifier()
+#Check if the string is a valid identifier:
+#print(b)
+
+#using islower
+a = "hello world"
+b = a.islower()
+#Check if all the characters in the text are in lower case:
+print(b)
