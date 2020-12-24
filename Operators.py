5a11cfb644b89ce77d362f17af5a7af384be7318
Shajidur Rahman <shajidurrahmansaad10@gmail.com>
2020-12-23 23:33:22 -0800

    01:30 pm

--- Operators.py
+++ Operators.py
@@ -1,1 +1,17 @@
-#Operator sector
+#list sector
+frout_name = ['banana','mango','sifat']
+#print(frout_name)
+
+
+#len list
+person_name = ['sifat','saad','jannat']
+#print(len(person_name))
+
+#type list
+person = ['saad','sifat']
+#print(type(person))
+
+#using -1
+a = ['saad','sifat','apu']
+#use -1 to get last item
+print(a[-1])
