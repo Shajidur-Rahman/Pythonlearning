3e7a37fb8d51e4ebee71b2e639f2c6f9c813a3d1
Shajidur Rahman <shajidurrahmansaad10@gmail.com>
2020-12-24 08:49:55 -0800

    11:50 pm

--- Operators.py
+++ Operators.py
@@ -14,4 +14,43 @@ person = ['saad','sifat']
 #using -1
 a = ['saad','sifat','apu']
 #use -1 to get last item
-print(a[-1])
+#print(a[-1])
+
+#changing the value
+a = ["sifat",'saad']
+a[0] = 'smrity'
+#print(a)
+
+#insert or adding
+a = ['saad','sifat']
+a.insert(1,'smrity')
+#print(a)
+
+#add 2 list
+a = ['sifat','sifat']
+b = ['smrity','hi']
+a.extend(b)
+#print(a)
+
+# removing . (you can use remove key or pop key or del key)
+a = ['saad','sifat']
+#del a[0]
+#print(a)
+
+#name = ['sifat','saad','smrity']
+#for i in name:
+    #print(i)
+#short cut
+#name = ['sifat','saad']
+#[print(i) for i in name]
+
+
+
+#using while loop in for loop
+name_person = ['saad', 'sifat', 'smrity', 'asjd']
+i = 0
+while i < len(name_person):
+    print(name_person[i]) #i am getting an error
+    i = i + 1
+
+#end of the day
